b25d4e2e1fc94b563bd57393c6a4519dfbfe19ab
paulo uilian <uilianlbarros@gmail.com>
2022-06-13 23:33:42 -0300

    Implementará arquivos inciais do projeto

---
+++ config/tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE "users" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"name" TEXT NOT NULL,
+	"password" TEXT NOT NULL UNIQUE,
+	"email" TEXT NOT NULL UNIQUE,
+	"image" TEXT,
+	"createdAt" timestamp NOT NULL DEFAULT NOW()
+); 
+
+
+CREATE TABLE "posts" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"userId" integer NOT NULL REFERENCES users(id),
+	"bodyPost" TEXT NOT NULL,
+	"link" TEXT NOT NULL,
+	"createdAt" timestamp NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE "hashtags" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"name" TEXT NOT NULL UNIQUE
+); 
+
+
+CREATE TABLE "hashtagsPosts" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"hashtagId" integer NOT NULL REFERENCES hashtags(id),
+	"postId" integer NOT NULL REFERENCES posts(id)
+); 
+
+
+CREATE TABLE "likesPosts" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"postId" integer NOT NULL REFERENCES posts(id),
+	"userId" integer NOT NULL REFERENCES users(id)
+);
