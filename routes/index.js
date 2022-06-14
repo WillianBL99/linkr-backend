d85ffe0cedc0b33070392f22adc8a55ee5b10dd1
carolxavier <xavier.biotec@gmail.com>
2022-06-14 20:17:15 -0300

    route to send list of the 10 most used hashtags implemented, GET('/hashtags')

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
-import {Router} from "express";
+import { Router } from "express";
 import timelineRouter from "./timelineRoute.js";
+import hashtagsRouter from "./hashtagsRoute.js";
 
 const router = Router();
+
 router.use(timelineRouter);
+router.use(hashtagsRouter);
 
 export default router;
