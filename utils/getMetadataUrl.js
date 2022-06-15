b94cf4301bfa69eb28b4ef1b00b09ca3e61ce487
paulo uilian <uilianlbarros@gmail.com>
2022-06-15 02:40:27 -0300

    Corrigirá problema com metadata
    Caso o metadata não seja econtrado retornara um objeto vasio

--- utils/getMetadataUrl.js
+++ utils/getMetadataUrl.js
@@ -1,6 +1,11 @@
 import urlMetadata from "url-metadata";
 
 export default async function getMetadataUrl(url) {
-  const {title, description, image} = await urlMetadata(url);
-  return {title, description, image};
+  try {
+    const {title, description, image} = await urlMetadata(url);
+    return {title, description, image};
+
+  } catch (e) {
+    return undefined;
+  }
 }
