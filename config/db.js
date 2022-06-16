0a2e5b9ab05a1ab6140d92a9db67397eba0627a0
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-06-15 23:01:13 -0300

    feat - login route

--- config/db.js
+++ config/db.js
@@ -1,41 +1,41 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-const { Pool } = pg;
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL
-};
-
-if(process.env.MODE === "PROD") {
-  configDatabase.ssl = {
-    rejectUnauthorized: false
-  }
-}
-
-const db = new Pool(configDatabase);
-export default db;
-
-
-// import pg from 'pg';
-
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 // const { Pool } = pg;
+// const configDatabase = {
+//   connectionString: process.env.DATABASE_URL
+// };
 
+// if(process.env.MODE === "PROD") {
+//   configDatabase.ssl = {
+//     rejectUnauthorized: false
+//   }
+// }
 
-// const user = 'postgres';
-// const password = 'Luvitas1992';
-// const host = '127.0.0.1';
-// const port = 5432;
-// const database = 'linkr_test';
-
-// const db = new Pool({
-//   user,
-//   password,
-//   host,
-//   port,
-//   database
-// });
-
-
+// const db = new Pool(configDatabase);
 // export default db;
+
+
+import pg from 'pg';
+
+
+const { Pool } = pg;
+
+
+const user = 'postgres';
+const password = 'Luvitas1992';
+const host = '127.0.0.1';
+const port = 5432;
+const database = 'linkr_test';
+
+const db = new Pool({
+  user,
+  password,
+  host,
+  port,
+  database
+});
+
+
+export default db;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import timelineRouter from "./timelineRoute.js";
 import hashtagsRouter from "./hashtagsRoute.js";
-
 import authRouter from "./authRouter.js";
-import timelineRouter from "./timelineRoute.js";
 import userRouter from "./userRouter.js";
 
 const router = Router();
