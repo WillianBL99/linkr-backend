d014365540a23cea6cabae13ab4fc552244caea3
carolxavier <xavier.biotec@gmail.com>
2022-06-16 16:32:06 -0300

    added getMetadata in getHashtagPosts on hashtagsController

--- controllers/hashtagsController.js
+++ controllers/hashtagsController.js
@@ -1,5 +1,6 @@
 import { getHashtagsRepository } from "../repositories/hashtagsRepositories.js";
 import { getHashtagPostsRepository } from "../repositories/hashtagsRepositories.js";
+import getMetadataUrl from "../utils/getMetadataUrl.js";
 
 export async function getHashtags(req, res) {
     try {
@@ -18,11 +19,22 @@ export async function getHashtags(req, res) {
 
 export async function getHashtagPosts(req, res) {
     const { hashtag } = req.params;
-
+    const posts = [];
     try {
         const postRows = await getHashtagPostsRepository(hashtag);
 
-        res.status(200).send(postRows);
+        for(let i = 0; i < postRows.length; i++){
+            const link = postRows[i].link;
+            delete postRows[i].link;
+            const metadata = await getMetadataUrl(link);
+
+            posts.push({
+                ...postRows[i],
+                metadata: {...metadata, link}
+            });
+        };
+
+        res.status(200).send(posts);
     } catch (error) {
         console.log("Error in getHashtagPosts", error);
         res.sendStatus(500);
