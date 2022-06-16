dc9e30b4187d13a44365c530bad0ce556a82de4d
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-06-16 13:37:41 -0300

    fix - db and env files

--- config/db.js
+++ config/db.js
@@ -17,25 +17,3 @@ const db = new Pool(configDatabase);
 export default db;
 
 
-// import pg from 'pg';
-
-
-// const { Pool } = pg;
-
-
-// const user = 'postgres';
-// const password = 'Luvitas1992';
-// const host = '127.0.0.1';
-// const port = 5432;
-// const database = 'linkr_test';
-
-// const db = new Pool({
-//   user,
-//   password,
-//   host,
-//   port,
-//   database
-// });
-
-
-// export default db;
