013c2be06e784b623fa3de85a87431a0d17c322f
paulo uilian <uilianlbarros@gmail.com>
2022-06-19 11:58:25 -0300

    Corrigirá validateToken

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { getSession } from "../repositories/sessionsRepositories.js";
 dotenv.config();
 
 export const validateToken = async (req, res, next) => {
@@ -10,11 +11,18 @@ export const validateToken = async (req, res, next) => {
 
     if (!token) return res.status(401).send({ message: "Token is missing" });
     try {
+        const {rows : [session]} = await getSession(token);
+        console.log(session);
+        if(!session) {
+            return res.status(401).send({ message: "Invalid token" });
+        }
+        
         const tokenData = jwt.verify(token, secret);
         const { userId } = tokenData;
         res.locals.tokenData = {userId};
   
         next();
+
     } catch (error) {
         console.log(error, "validateToken");
         res.status(500).send(error);
