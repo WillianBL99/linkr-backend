45fa83ca19be2ef12352adca3c60cda85589ec3e
paulo uilian <uilianlbarros@gmail.com>
2022-06-19 16:04:32 -0300

    Corrigirá getHashtags

--- repositories/hashtagsRepositories.js
+++ repositories/hashtagsRepositories.js
@@ -5,6 +5,8 @@ export async function getHashtagsRepository() {
         SELECT h."name", COUNT(hp."hashtagId") as count
         FROM hashtags h
         JOIN "hashtagsPosts" hp ON h.id = hp."hashtagId"
+        JOIN "posts" p ON hp."postId" = p.id
+        WHERE p."statusId" <> 3
         GROUP BY h."name" 
         ORDER BY count desc limit 10
     `);
