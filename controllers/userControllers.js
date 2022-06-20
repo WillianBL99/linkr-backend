7e376b08a9dd4d48c8d5f72f7d7e93a4f317f671
Maria Luiza <maria.lmm.975@gmail.com>
2022-06-20 10:03:15 -0300

    feat: add handlePostsData

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,10 +3,12 @@ import SqlString from "sqlstring";
 import userRepository from "./../repositories/userRepositories.js";
 import { getPostsByFilter } from "../repositories/postsRepository.js";
 import { infoLikes } from "../repositories/timelineRepositories.js";
+import handlePostsData from "../utils/handlePostsData.js";
 
 export async function getUserPosts(req, res) {
     const id = parseInt(req.params.id);
     const posts = req.query.posts;
+    const {tokenData} = res.locals
 
     try {
         let user = await userRepository.getUserById(id);
@@ -23,34 +25,9 @@ export async function getUserPosts(req, res) {
 
         const filter = `WHERE "userId" = ${SqlString.escape(id)} AND "statusId" <> 3`;
 
-        const userPosts = await getPostsByFilter(filter);
+        let userPosts = await getPostsByFilter(filter);
 
-        for (let i = 0; i<userPosts.length; i++) {
-            const post = userPosts[i];
-            const {
-                link,
-                title,
-                imageLink,
-                name,
-                image,
-                postId,
-                userId,
-                postBody,
-                postStatus,
-            } = post;
-            const metadata = { link, title, image: imageLink };
-
-            userPosts[i] = {
-                metadata,
-                name,
-                image,
-                postId,
-                userId,
-                postBody,
-                postStatus,
-                infoLikes: await infoLikes(userId, postId)
-            };
-        }
+        userPosts = await handlePostsData(tokenData.userId, userPosts);
         
         res.status(200).send(userPosts);
     } catch (e) {
